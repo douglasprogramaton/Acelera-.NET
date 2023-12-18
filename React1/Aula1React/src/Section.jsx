ebf069fbd764ef4f7a789e5c306f37e24528b151
Douglas Santos <douglassantosprogramaton@gmail.com>
2023-12-18 15:52:47 -0300

    Links de Games

--- React1/Aula1React/src/Section.jsx
+++ React1/Aula1React/src/Section.jsx
@@ -21,12 +21,12 @@ export default function Section(props) {
                         <p>{props.SubTitulo}</p>
                         
                         <ul class="film-list">
-                                <li><a href="https://playvalorant.com/pt-br/?gad_source=1&gclid=Cj0KCQiAj_CrBhD-ARIsAIiMxT8McAf4wU5mNZCsQ6ohb-AE9I1PB1YcZKLDApGVlY8AjvoyiKSTsLgaAnU6EALw_wcB&gclsrc=aw.ds" target="_blank"><img src="./Imagens/VAVA.JPG"/></a></li>
-                                <li><a href="https://store.steampowered.com/app/730/CounterStrike_2/" target="_blank"><img src="./Imagens/CS.JPG"/></a></li>
-                                <li><a href="https://www.rockstargames.com/br/gta-v"target="_blank"><img src="./Imagens/GTA.JPG"/></a></li>
-                                <li><a href="https://www.leagueoflegends.com/pt-br/"target="_blank"><img src="./Imagens/LOL.JPG"/></a></li>
-                                <li><a href="https://www.fortnite.com/?lang=pt-BR"target="_blank"><img src="./Imagens/Fortnite.JPG"/></a></li>
-                                <li><a href="https://www.callofduty.com/br/pt/warzone"target="_blank"><img src="./Imagens/COD.JPG"/></a></li>                    
+                                <li><a href="https://www.youtube.com/watch?v=UHFha439Rt8" target="_blank"><img src="./Imagens/VAVA.JPG"/></a></li>
+                                <li><a href="https://www.youtube.com/watch?v=nSE38xjMLqE" target="_blank"><img src="./Imagens/CS.JPG"/></a></li>
+                                <li><a href="https://www.youtube.com/watch?v=QkkoHAzjnUs"target="_blank"><img src="./Imagens/GTA.JPG"/></a></li>
+                                <li><a href="https://www.youtube.com/watch?v=vzHrjOMfHPY"target="_blank"><img src="./Imagens/LOL.JPG"/></a></li>
+                                <li><a href="https://www.youtube.com/watch?v=D-VNKfmm_ho"target="_blank"><img src="./Imagens/Fortnite.JPG"/></a></li>
+                                <li><a href="https://www.youtube.com/watch?v=0E44DClsX5Q"target="_blank"><img src="./Imagens/COD.JPG"/></a></li>                    
                          </ul>
 
                     </div>
