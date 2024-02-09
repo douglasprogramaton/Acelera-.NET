842ba28e95218ba2f33b8512d2005fd6e622563f
Douglas Santos <douglassantosprogramaton@gmail.com>
2024-02-09 09:34:31 -0300

    dell

--- mvc/WebApplication2/Controllers/EmprestimoController.cs
+++ mvc/WebApplication2/Controllers/EmprestimoController.cs
@@ -1,4 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using WebApplication2.Models;
+
+//namespace WebApplication2.Controllers
+//{
+//    public class EmprestimoController : Controller
+//    {
+//        Data.ContextoDB contexto = new Data.ContextoDB();
+
+//        public IActionResult Index()
+//        {
+//            var emprestimo = contexto.Emprestimos.ToList();
+
+//            return View(emprestimo);
+
+//        }
+//        [HttpPost]
+//        public IActionResult Index(EmprestimoModel empretimossss)
+//        {
+//            contexto.Emprestimos.Add(empretimossss);
+//            contexto.SaveChanges();
+//            var emprestimosDB = contexto.Emprestimos.ToList();
+//            return View(emprestimosDB);
+//        }
+
+//        public IActionResult Cadastra()
+//        {
+
+//            var emprestimoModel = new EmprestimoModel(); // inicialize o objeto se necessário
+//            return View(emprestimoModel);
+//        }
+//        [HttpPost]
+//        public IActionResult Cadastra(EmprestimoModel rtd)
+//        {
+//            contexto.Add(rtd);
+//            return View(rtd);
+//        }
+
+//    }
+//}
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using WebApplication2.Models;
 
 namespace WebApplication2.Controllers
@@ -10,10 +51,9 @@ namespace WebApplication2.Controllers
         public IActionResult Index()
         {
             var emprestimo = contexto.Emprestimos.ToList();
-
             return View(emprestimo);
-
         }
+
         [HttpPost]
         public IActionResult Index(EmprestimoModel empretimossss)
         {
@@ -25,14 +65,38 @@ namespace WebApplication2.Controllers
 
         public IActionResult Cadastra()
         {
-            return View();
-        }
-        [HttpPost]
-        public IActionResult Cadastra(EmprestimoModel rtd)
-        {
-            contexto.Add(rtd);
-            return View(rtd);
+            var emprestimoModel = new EmprestimoModel();
+            return View(emprestimoModel);
         }
 
+        [HttpPost]
+        public IActionResult Cadastra(EmprestimoModel rtd, string nomeLivro, string nomeUsuario)
+        {
+      
+            if (string.IsNullOrEmpty(nomeLivro))
+            {
+                ModelState.AddModelError("NomeLivro", "Nome do livro é obrigatório");
+                return View(rtd);
+            }
+
+         
+            var livro = contexto.Livros.FirstOrDefault(l => l.LIVRO == nomeLivro);
+            if (livro == null)
+            {
+                ModelState.AddModelError("NomeLivro", "Livro não encontrado");
+                return View(rtd);
+            }
+
+
+            rtd.IDLIVRO = livro.COD;
+            rtd.IDUSUARIO = nomeUsuario;
+            rtd.DTEMPRESTIMO = DateTime.Now;
+
+            // Adicionar o empréstimo ao contexto e salvar as mudanças
+            contexto.Emprestimos.Add(rtd);
+            contexto.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
--- mvc/WebApplication2/obj/Debug/net6.0/WebApplication2.AssemblyInfo.cs
+++ mvc/WebApplication2/obj/Debug/net6.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c943b27da2dda2d1e2068d987ac6f98b06dfb7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1692f1b2281faea2642cf0e3f5b94d0cf3e2dfec")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
